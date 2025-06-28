2f9570ad17153db4a8d91b1c8a3a3f49b703a09e
Jon Arbuckle <johnderugo@gmail.com>
2025-06-28 19:55:34 -0400

    Tenth commit, please tell me the website is fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/weight_loss_app/',
+  base: '/Weight_Change_App/',
 })
